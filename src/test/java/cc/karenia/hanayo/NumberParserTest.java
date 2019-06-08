5f7d7636465ecd6c712dc412ebce38c92aa07826
Hu Mingzhi <746354353Wqq.com>
2019-06-08 13:03:45 +0800

    test

--- src/test/java/cc/karenia/hanayo/NumberParserTest.java
+++ src/test/java/cc/karenia/hanayo/NumberParserTest.java
@@ -11,34 +11,77 @@ public class NumberParserTest {
 
   @Test
   public void TestRegularNumber() throws HoconParseException {
-    var parseResult = HoconParser.of("10000").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, true);
-    assertEquals(parseResult.result.value, "10000");
-    assertEquals(parseResult.result.asInt(), 10000);
+	  var number1 = "1000 ".toCharArray();
+	    var number2 = "+1200 ".toCharArray();
+	    var number3 = "-2000 ".toCharArray();
+	    var number4 = "521.1 ".toCharArray();
+	    var number5 = "112E3 ".toCharArray();
+	    var number6 = "121e2 ".toCharArray();
+	    var number7 = "+521.1 ".toCharArray();
+	    var number8 = "-123E4 ".toCharArray();
+	    var number9 = "+121e3 ".toCharArray();
 
-    parseResult = HoconParser.of("10000e").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, true);
-    assertEquals(parseResult.result.value, "10000");
+	    var parseResult = HoconParser.parseNumber(number1, 0);
+	    assertEquals(parseResult.result.isInteger, true);
+	    assertEquals(parseResult.result.value, "1000");
+	    assertEquals(parseResult.result.asInt(), 1000);
 
-    parseResult = HoconParser.of("10000e5").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, false);
-    assertEquals(parseResult.result.value, "10000e5");
-    assertEquals(parseResult.result.asDouble(), 1000000000d, 1);
+	    parseResult = HoconParser.parseNumber(number2, 0);
+	    assertEquals(parseResult.result.isInteger, true);
+	    assertEquals(parseResult.result.value, "+1200");
+	    assertEquals(parseResult.result.asInt(), 1200);
 
-    parseResult = HoconParser.of("10000.1").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, false);
-    assertEquals(parseResult.result.value, "10000.1");
-    assertEquals(parseResult.result.asDouble(), 10000.1, 0.001);
+	    parseResult = HoconParser.parseNumber(number3, 0);
+	    assertEquals(parseResult.result.isInteger, true);
+	    assertEquals(parseResult.result.value, "-2000");
+	    assertEquals(parseResult.result.asInt(), -2000);
 
-    parseResult = HoconParser.of("10000.1e5").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, false);
-    assertEquals(parseResult.result.value, "10000.1e5");
-    assertEquals(parseResult.result.asDouble(), 1000010000d, 1);
+	    parseResult = HoconParser.parseNumber(number4, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "521.1");
+	    assertEquals(parseResult.result.asDouble(), 521.1, 0.1);
 
-    parseResult = HoconParser.of("-10000.1e5").parseNumber(0);
-    assertEquals(parseResult.result.isInteger, false);
-    assertEquals(parseResult.result.value, "-10000.1e5");
-    assertEquals(parseResult.result.asDouble(), -1000010000d, 1);
+	    parseResult = HoconParser.parseNumber(number5, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "112E3");
+	    assertEquals(parseResult.result.asDouble(), 112000, 1);
+	    
+	    parseResult = HoconParser.parseNumber(number6, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "121e2");
+	    assertEquals(parseResult.result.asDouble(), 12100, 1);
+	    
+	    parseResult = HoconParser.parseNumber(number7, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "+521.1");
+	    assertEquals(parseResult.result.asDouble(), 521.1, 0.1);
+	    
+	    parseResult = HoconParser.parseNumber(number8, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "-123E4");
+	    assertEquals(parseResult.result.asDouble(), -1230000, 1);
+	    
+	    parseResult = HoconParser.parseNumber(number9, 0);
+	    assertEquals(parseResult.result.isInteger, false);
+	    assertEquals(parseResult.result.value, "+121e3");
+	    assertEquals(parseResult.result.asDouble(), 121000, 1);
+	  }
+
+	  @Test
+	  public void testSubstitution() throws HoconParseException {
+	    var sub1 = "${a.c}".toCharArray();
+	    var parseResult = HoconParser.parseSubstitution(sub1, 0);
+	    assertEquals(parseResult.parseSuccess, true);
+	    assertEquals(parseResult.result.isDetermined, true);
+	    assertEquals(parseResult.result.path.name, "a");
+	    assertEquals(parseResult.result.path.next.name, "c");
+
+	    var sub2 = "${?b.c}".toCharArray();
+	    parseResult = HoconParser.parseSubstitution(sub2, 0);
+	    assertEquals(parseResult.parseSuccess, true);
+	    assertEquals(parseResult.result.isDetermined, false);
+	    assertEquals(parseResult.result.path.name, "b");
+	    assertEquals(parseResult.result.path.next.name, "c");
   }
 
   @Test
---
+++ src/test/java/cc/karenia/hanayo/UnquotedStringTest.java
@@ -0,0 +1,5 @@
+package cc.karenia.hanayo;
+
+public class UnquotedStringTest {
+
+}
